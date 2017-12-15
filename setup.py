d09e5f894d195969465ed5d5fa8fa71cbbc0b9ef
indivisibleatom <mukulsati@gmail.com>
2017-12-14 20:35:04 -0500

    Adding author and module name

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from Cython.Build import cythonize
 import eigency
 
 setup(
+    name = 'pysophus',
+    author = 'Arnaud Tanguy',
     ext_modules = cythonize([Extension("sophus", 
         sources=["sophus.pyx"],
         include_dirs = [".", "/usr/include/eigen3", "./Sophus"] + eigency.get_includes(include_eigen=False),
